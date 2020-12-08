7ea90dc001c2be85e67a73aba56b170bcde174ea
jannajothomas <jannajothomas@gmail.com>
2020-12-07 21:00:35 -0500

    Initial Commit

---
+++ Cooperation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cooperation
+//
+//  Created by Susan Jensen on 12/7/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
