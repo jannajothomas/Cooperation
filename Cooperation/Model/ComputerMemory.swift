3aeb012ed3a72b7ef44798e28dba6a198a2179db
jannajothomas <jannajothomas@gmail.com>
2021-02-26 19:46:57 -0500

    Finalize animation fixes

--- Cooperation/Model/ComputerMemory.swift
+++ Cooperation/Model/ComputerMemory.swift
@@ -12,62 +12,11 @@ struct ComputerMemory{
         }
     }
     
-    mutating func cardDrawn(player: Int, cardLocation: Int, cardToRemove: Card){
-        if player == 0{
-            cardPossibilities[cardLocation] = newCardPossibilities
-        }
-        else{
-            cardShown(knownCard: cardToRemove)
-        }
-        printComputerCardMemory()
-    }
-    
-    mutating func cardPlayedOrDiscarded(player: Int, cardLocation: Int, cardPlayed: Card){
-        if player == 0{
-            cardShown(knownCard: cardPlayed)
-            cardPossibilities[cardLocation] = newCardPossibilities
-        }
-        printComputerCardMemory()
-    }
-    
-    /*Cards can be shown when the opposing player draws a card or when the comptuer discards or plays a card. */
-    mutating func cardShown (knownCard: Card){
-        //Remove the card from generic card possibility array
-        newCardPossibilities = removeCardFromArray(card: knownCard, array: newCardPossibilities)
-        //remove it from each specific array if it exists
-        for count in 0...4{
-            cardPossibilities[count] = removeCardFromArray(card: knownCard, array: cardPossibilities[count])
-        }
-    }
-    
-    /*If card exists in array, remove it.  If not, return the original array */
-    func removeCardFromArray(card: Card, array: [Card])->[Card]{
-        var newArray = array
-        for count in 0...array.count - 1{
-            if(array[count] == card){
-                //print("count", count)
-                
-                newArray.remove(at: count)
-                
-                return newArray
+    func printCardPossibilities(){
+        for column in 0...cardPossibilities.count - 1{
+            for row in 0...cardPossibilities[column].count - 1{
+                print(cardPossibilities[count])
             }
-        }
-        return newArray
-    }
-    
-    func printComputerCardMemory(){
-        for count in 0...4{
-            var firstColor = cardPossibilities[count][0].col
-            for card in 0...cardPossibilities[count].count - 1{
-                if cardPossibilities[count][card].col != firstColor{
-                    //print()
-                    firstColor = cardPossibilities[count][card].col
-                }
-                //print(cardPossibilities[count][card],terminator:"")
-                
-                
-            }
-            //print()
         }
     }
 }
--- Cooperation/Model/ComputerPlayer.swift
+++ Cooperation/Model/ComputerPlayer.swift
@@ -9,14 +9,12 @@
 import Foundation
 
 struct ComputerPlayer{
-   var computerMemory = ComputerMemory()
-   var computerHand = 0
+    var computerMemory = ComputerMemory()
+    var computerHand = 0
     var otherPlayersHand = Array(repeating: Card(), count: 5)
     var stacks = Array(repeating: Array(repeating: Card(), count: 5), count: 5)
     var discards = Array(repeating:  Array(repeating: Card(), count: 10), count: 5)
 
-
-    //TODO: Make this an enum?
     var action = ""
     var cardToAct = -1
     var stackToActOn = -1
@@ -25,11 +23,15 @@ struct ComputerPlayer{
     
     func updateAI(){
         
-        
-        
     }
     
     mutating func playBestMove(){
+        //get points for play
+        
+        //get points for discard
+        
+        //get points for hint
+        
         //TODO: Replace hard wired play action with a informed move
         action = "play"
         cardToAct = 1
@@ -40,5 +42,25 @@ struct ComputerPlayer{
        // print("Computer playBestMove")
         ComputerPlayer.totalTurns+=1
     }
+    
+    //How do i make this meerly a math problem.
+        //1. Build piles
+                //not throw away cards that are the last ones remaining
+                //get cards on stacks asap
+    
+    
+    
+    //Best Moves
+        //My hand facing
+        //Opponent facing
+    
+    //Conditional Moves
+        //My hand facing
+        //Opponent facing
+    
+    //Moves of despiration
+        //best probability
+    
+    
 }
 
